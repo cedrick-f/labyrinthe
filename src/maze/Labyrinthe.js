7aa348fb2a28c0e981a06e145b5db73215a4e90c
MarkkBerg <mamarthevdb63@gmail.com>
2021-03-11 09:54:09 +0100

    mursCellule + voisinsCellule
    Avancement du fichier Labyrinthe

--- src/maze/Labyrinthe.js
+++ src/maze/Labyrinthe.js
@@ -18,6 +18,46 @@ export class Labyrinthe {
         
     }
 	
+	/** tester les résultats et changer les + et -
+	*/
+	mursCellule(s){
+		let ws = s[1]
+		let hs = s[0]
+		let l = []
+		if (ws !== 0) {
+			l.push([[hs, ws],[hs, ws + 1]])
+		}
+		if (ws !== this.width-1) {
+			l.push([[hs, ws],[hs, ws + 1]])
+		}
+		if (hs !== 0) {
+			l.push([[hs, ws],[hs + 1, ws]])
+		}
+		if (hs !== this.height-1) {
+			l.push([[hs, ws],[hs + 1, ws]])
+		}
+		return l
+	}
+	
+	voisinsCellule(s) {
+		let ws = s[0]
+		let hs = s[1]
+		let l = []
+		if (ws !== 0) {
+			l.push([[hs, ws + 1]])
+		}
+		if (ws !== this.width) {
+			l.push([[hs, ws + 1]])
+		}
+		if (hs !== 0) {
+			l.push([[hs + 1, ws]])
+		}
+		if (hs !== this.height) {
+			l.push([[hs - 1, ws]])
+		}
+		return l
+	}
+	
 	/**
      * @param {number} ax 
      * @param {number} ay 
@@ -29,3 +69,5 @@ export class Labyrinthe {
 		return true
 	}
 }
+
+
