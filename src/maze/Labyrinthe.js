9337a0f3025c72f7a532d851ce706d61bed8f8db
MarkkBerg <mamarthevdb63@gmail.com>
2021-03-16 14:57:24 +0100

    Avancée de Labyrinthe

--- src/maze/Labyrinthe.js
+++ src/maze/Labyrinthe.js
@@ -3,23 +3,23 @@ import {Graphe} from "./Graphe.js"
 import {Coords, Mur} from "../util/Coords.js";
 
 export class Labyrinthe {
-    /**
-     * @param {number} width 
-     * @param {number} height 
-     * @param {('fusion'|'prim'|'aldous-broder')} generatorName
-     */
-    constructor(width, height, generatorName) {
-        this.width = width
-        this.height = height
+	/**
+	 * @param {number} width 
+	 * @param {number} height 
+	 * @param {('fusion'|'prim'|'aldous-broder')} generatorName
+	 */
+	constructor(width, height, generatorName) {
+		this.width = width
+		this.height = height
 		this.graphe = new Graphe()
-    }
+	}
 
-    /**
-     * @param {function (Mur): void} [callback]
-     */
-    generate(callback) {
-        
-    }
+	/**
+	 * @param {function (Mur): void} [callback]
+	 */
+	generate(callback) {
+	    
+	}
 
 	/**
 	 * @param {number} x
@@ -43,14 +43,14 @@ export class Labyrinthe {
 		return l
 	}
 
-    /**
-     * @param {number} x
-     * @param {number} y
-     * @return {Mur[]}
-     */
-    mursCellule(x, y) {
-        return this.voisinsCellule(x, y).map(voisin => new Mur(new Coords(x, y), voisin));
-    }
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 * @return {Mur[]}
+	 */
+	mursCellule(x, y) {
+		return this.voisinsCellule(x, y).map(voisin => new Mur(new Coords(x, y), voisin));
+	}
 
 	/**
 	 * @param {Coords} a
@@ -66,14 +66,37 @@ export class Labyrinthe {
 	
 	/**
 	 * Retourne true s'il y a pas d'arête au sens graphe entre deux cellules
-     *
+	 *
 	 * @param {number} ax
-     * @param {number} ay 
+	 * @param {number} ay 
 	 * @param {number} bx
 	 * @param {number} by
 	 * @return {boolean}
-     */
+	 */
 	murEntre(ax, ay, bx, by) {
 		return !this.graphe.voisins(Coords.identifiant(ax, ay, this)).has(Coords.identifiant(bx, by, this))
 	}
+
+	/**aller d'un point a à un point b avec le plus court chemin (methode d'axel dans Graphe)
+	 * quel point a, quel point b ? (les plus éloignés possibles)
+	 *
+	 * fonction à modifier
+	parcours_largeur(from) {
+		if (!(from in this.A)) {
+			return []
+		}
+		let r = []
+        let s = []
+        s.push(from)
+        while (s.length) {
+            let p = s.shift()
+            if (!(r.includes(p))) {
+                r.push(p)
+                for (let e of this.voisins(p)) {
+                    s.push(e)
+				}
+			}
+		}
+        return r
+     */
 }
