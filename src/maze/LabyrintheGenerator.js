a06d37538c85051b86135af464f185e963b3c84a
MarkkBerg <mamarthevdb63@gmail.vcom>
2021-03-19 09:57:27 +0100

    Tout ensemble entre Aldous et Fusion !

--- src/maze/LabyrintheGenerator.js
+++ src/maze/LabyrintheGenerator.js
@@ -13,25 +13,61 @@ export function generatorFromName(name, labyrinthe) {
 	if (name === 'aldous-broder') {
 		return new AldousGenerator(labyrinthe)
 	}
+	if (name === 'fusion') {
+		return new Fusion(labyrinthe)
+	}
     return {
         next: () => new Mur(new Coords(0, 0), new Coords(0, 1)),
         hasNext: () => false
     }
 }
 
-/*class Fusion {
-	constructor(x, y) {
-		let l = []
-		this x = x
-		this y = y
+class Fusion {
+	constructor(labyrinthe) {
+		this.labyrinthe = labyrinthe
+		this.construire_fusion()
 	}
 		
-	construire_fusion(x, y){
-		for (let x = 0; x < labirynthe.width; x++) {
-			
+	construire_fusion(){
+		this.grille = []
+		let i = 0
+		for (let y = 0; y < this.labyrinthe.height; y++) {
+			let l = []
+			for (let x = 0; x < this.labyrinthe.width; x++) {
+				l.push(Coords.identifiant(x, y, this.labyrinthe))
+			}
+			this.grille.push(l)
 		}
+		
+		debugger
 	}
-}*/
+	
+	next() {
+		const x = randomInt(this.labyrinthe.width)
+		const y = randomInt(this.labyrinthe.height)
+		let cellule = new Coords(x, y)
+		let voisins = this.labyrinthe.voisinsCellule(x, y)
+		let voisinRandom = randomChoice(voisins)
+		let celluleIdentifiant = this.grille[y][x]
+		let voisinIdentifiant = this.grille[voisinRandom.y][voisinRandom.x]
+		if (celluleIdentifiant !== voisinIdentifiant) {
+			this.labyrinthe.ouvrir_passage(cellule, voisinRandom)
+		}
+		
+	}
+	
+	hasNext() {
+		for (let ligne of this.grille) {
+			for (let cellule of this.grille) {
+				if (cellule > 0) {
+					return true
+				}
+				
+			}
+		}
+		return false
+	}
+}
 
 class RandomGenerator {
 
