bd1d64dc1e00afdfbb0bf2fedd41f50379cee968
syldium <syldium@mailo.com>
2021-03-23 19:02:34 +0100

    feat(random): fonction pour mélanger un tableau

--- src/util/Random.js
+++ src/util/Random.js
@@ -18,3 +18,20 @@ export function randomInt(max) {
 export function randomChoice(array) {
 	return array[randomInt(array.length)]
 }
+
+/**
+ * Mélange aléatoirement les éléments du tableau sur place.
+ *
+ * @param {T[]} array
+ * @return {T[]}
+ * @template T
+ */
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+}
