566a6feacfba5d659a7523298a59923b1c58b582
syldium <syldium@mailo.com>
2021-03-25 19:28:14 +0100

    refactor(modal): accessibilité clavier, JS vanilla

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import { Controller } from './Controller.js'
+import {bindModalEvents} from './vue/modal.js';
 
 new Controller(document.querySelector('.grid'))
+
+bindModalEvents(
+    document.querySelectorAll('.modal-action'),
+    document.querySelector('#modal-container')
+)
---
+++ src/vue/modal.js
@@ -0,0 +1,71 @@
+/** @type {null|HTMLElement} */
+let currentModal = null
+/** @type {Object.<string, string>} */
+const cache = {}
+
+/**
+ * Affiche la boîte modale.
+ *
+ * @param {Event} event
+ * @param {HTMLElement} modal Conteneur global
+ */
+async function openModal(event, modal) {
+    event.preventDefault()
+    const target = event.target.getAttribute('href')
+    modal.lastElementChild.lastElementChild.innerHTML = await loadModal(target)
+    modal.setAttribute('aria-modal', 'true')
+    modal.removeAttribute('hidden')
+    modal.querySelector('.close').focus()
+    currentModal = modal
+}
+
+/**
+ * Ferme la boîte modale actuellement ouverte.
+ *
+ * @param {Event} event
+ */
+function closeModal(event) {
+    if (currentModal === null) {
+        return
+    }
+    event.preventDefault()
+    currentModal.setAttribute('hidden', 'hidden')
+    currentModal.removeAttribute('aria-modal')
+    currentModal = null
+}
+
+/**
+ * @param {string} url
+ * @return {Promise<string>}
+ */
+async function loadModal(url) {
+    if (cache[url]) {
+        return cache[url]
+    }
+    const html = await fetch(url).then(response => response.text())
+    cache[url] = html
+    return html
+}
+
+/**
+ * @param {NodeList} nodeList
+ * @param {HTMLElement} modalContainer
+ */
+export function bindModalEvents(nodeList, modalContainer) {
+    nodeList.forEach(function (link) {
+        link.addEventListener('click', event => openModal(event, modalContainer))
+    })
+
+    const close = modalContainer.querySelector('.close')
+    close.addEventListener('click', closeModal)
+    close.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            closeModal(event)
+        }
+    })
+    window.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape' || event.key === 'Esc') {
+            closeModal(event)
+        }
+    })
+}
