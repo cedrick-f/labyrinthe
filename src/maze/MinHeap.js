e5d5c9ccee1b890a05bfc6a007818a88b9cce895
syldium <syldium@mailo.com>
2022-01-18 14:15:47 +0100

    fix(tas): utilise comme racine l'élément à l'indice 0

--- src/maze/MinHeap.js
+++ src/maze/MinHeap.js
@@ -33,10 +33,10 @@ export class MinHeap {
     queue(val, priority) {
         this.heap.push(new Node(val, priority))
         let current = this.heap.length - 1
-        while (current > 0 && this.heap[current >> 1].priority > this.heap[current].priority) {
+        while (current > 0 && this.heap[(current - 1) >> 1].priority > this.heap[current].priority) {
             // On fait remonter le nœud tant que son parent a une priorité plus importante.
-            this._swap(current, current >> 1)
-            current >>= 1
+            this._swap(current, (current - 1) >> 1)
+            current = (current - 1) >> 1
         }
     }
 
@@ -55,23 +55,21 @@ export class MinHeap {
 
         // On utilise comme nouvelle racine le nœud en dernière position du tas
         this.heap[0] = this.heap.pop()
-        let index = 1
-        while (index < this.heap.length) {
+        let current = 0
+        while (this._hasLeftChild(current)) {
             // Que l'on fait descendre dans l'arbre tant qu'un des nœuds enfants a une priorité plus faible
-            const parent = this.heap[index >> 1].priority
-            const left = this.heap[index].priority
-            const right = (index + 1) < this.heap.length ? this.heap[index + 1].priority : Number.POSITIVE_INFINITY
-            if (parent < left && parent < right) {
-                return root.val
+            let smallerChildIndex = (current << 1) + 1
+            if ((smallerChildIndex + 1) < this.heap.length && this.heap[smallerChildIndex + 1].priority < this.heap[smallerChildIndex].priority) {
+                smallerChildIndex += 1
             }
 
             // Échange avec le fils qui a la priorité la plus faible
-            if (left < right) {
-                this._swap(index >> 1, index)
+            if (this.heap[current].priority > this.heap[smallerChildIndex].priority) {
+                this._swap(current, smallerChildIndex)
+                current = smallerChildIndex
             } else {
-                this._swap(index >> 1, index + 1)
+                return root.val
             }
-            index <<= 1
         }
         return root.val
     }
@@ -83,6 +81,15 @@ export class MinHeap {
      */
     empty() {
         return this.heap.length === 0;
+    }
+
+    /**
+     * @param {number} index
+     * @return {boolean}
+     * @private
+     */
+    _hasLeftChild(index) {
+        return ((index << 1) + 1) < this.heap.length;
     }
 
     /**
